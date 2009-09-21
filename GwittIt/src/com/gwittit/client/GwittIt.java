91970d2f7b0c55ba844fae059a8caa6e64997acb
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2009-09-21 17:34:30 +0000

    updates

--- GwittIt/src/com/gwittit/client/GwittIt.java
+++ GwittIt/src/com/gwittit/client/GwittIt.java
@@ -2,30 +2,20 @@ package com.gwittit.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.gwittit.client.events.AppEvents;
 import com.gwittit.client.events.AppEvents.Event;
+import com.gwittit.client.facebook.FacebookCallback;
 import com.gwittit.client.facebook.FacebookConnectInit;
+import com.gwittit.client.facebook.UserInfo;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class GwittIt implements EntryPoint {
-
 	
 	private Frontpage frontpage;
 	
@@ -37,18 +27,19 @@ public class GwittIt implements EntryPoint {
 	
 		final HandlerManager eventBus = new HandlerManager ( null );
 
-		new FacebookConnectInit(Config.API_KEY, "/xd_receiver.htm") {
-			@Override
-			public void onLogin () {
-				AppEvents loginEvent = new AppEvents ( Event.LOGIN );
-				eventBus.fireEvent( loginEvent );
-				Window.alert( "You logged in");
+		FacebookConnectInit fbInit = FacebookConnectInit.newInstance();
+		fbInit.init( Config.API_KEY, "/xd_receiver.htm", new FacebookCallback () {
+			public void onError(JSONObject o) {
+				Window.alert ( "Error login");
 			}
-			
-		};
-
+			public void onSuccess(JSONObject o) {
+				GWT.log( "User logged in sesskey = " + UserInfo.getSessionKey(), null);
+				 AppEvents loginEvent = new AppEvents ( Event.LOGIN );
+                 eventBus.fireEvent( loginEvent );
+			} 
+		});
+		
 		frontpage = new Frontpage ( eventBus );
-	
 		RootPanel.get().add(frontpage);
 	}
 }
