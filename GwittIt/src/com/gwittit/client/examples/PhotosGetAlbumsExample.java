35c65749cb420d680cbf66258425f74b96efa2a5
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2009-09-22 08:29:41 +0000

    example classes

---
+++ GwittIt/src/com/gwittit/client/examples/PhotosGetAlbumsExample.java
@@ -0,0 +1,77 @@
+package com.gwittit.client.examples;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.gwittit.client.facebook.FacebookApi;
+import com.gwittit.client.facebook.commands.PhotosGetAlbums;
+import com.gwittit.client.facebook.entities.Album;
+import com.gwittit.client.facebook.xfbml.FbPhoto;
+import com.gwittit.client.facebook.xfbml.Xfbml;
+
+public class PhotosGetAlbumsExample extends Example {
+	
+	
+	private VerticalPanel outer = new VerticalPanel ();
+
+	private Image loader = new Image ( "/loader.gif");
+	
+	/**
+	 * @param apiClient
+	 */
+	public PhotosGetAlbumsExample ( FacebookApi apiClient ) {
+		
+		
+		outer.getElement().setId("PhotosGetAlbumsExample");
+		outer.add ( new HTML ( "<h1>Photos...</h1>"));
+		outer.add ( loader );
+		
+		Map<String,String> params = new HashMap<String,String> ();
+		
+		
+		new PhotosGetAlbums (params, apiClient) {
+			
+			@Override
+			public void onSuccess(List<Album> albums) {
+		
+				outer.remove( loader );
+				
+				outer.add ( new HTML ("<h3> Albums </h3>" ) );
+				FlowPanel albumsPanel = new FlowPanel ();
+				albumsPanel.addStyleName ( "albumsPanel");
+				
+				for ( Album album : albums ) {
+					FlowPanel tmp = new FlowPanel ();
+					tmp.addStyleName ( "album" );
+					tmp.add ( new HTML ( album.getName() ) ) ;
+					FbPhoto photo = new FbPhoto ( album.getCoverPid() );
+					photo.getElement().setAttribute("size", "thumb");
+					tmp.add ( photo );
+
+					albumsPanel.add( tmp );
+				}
+				
+				outer.add ( albumsPanel );
+				Xfbml.parse();
+
+			}
+		};
+		
+		initWidget( outer );
+		
+	}
+
+	@Override
+	public String getInfo() {
+		return "Demonstrates the API call photos.getAlbums";
+	}
+
+}
