92989465a3e95d02e55f852081f4f6ead28dc7db
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2009-10-06 08:31:26 +0000

    new entites

---
+++ GwittIt/src/com/gwittit/client/facebook/entities/Notification.java
@@ -0,0 +1,11 @@
+package com.gwittit.client.facebook.entities;
+
+import java.util.List;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONValue;
+
+public class Notification {
+	
+
+}
