eeeb46c676f86becdf276c84c5481606982eb11a
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2009-10-06 21:18:26 +0000

    notifications.getList

--- GwittIt/src/com/gwittit/client/example/Notifications_getList.java
+++ GwittIt/src/com/gwittit/client/example/Notifications_getList.java
@@ -1,6 +1,7 @@
 package com.gwittit.client.example;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.google.gwt.json.client.JSONValue;
@@ -9,6 +10,7 @@ import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwittit.client.facebook.FacebookApi.NotificationsGetListParams;
+import com.gwittit.client.facebook.entities.Notification;
 
 /**
  * 
@@ -28,18 +30,28 @@ public class Notifications_getList extends Example {
 		Map<String,String> params = new HashMap<String,String> ();
 		params.put(NotificationsGetListParams.include_read.toString(), "true");
 		//params.put("session_key", UserInfo.getSessionKey());
-		apiClient.notifications_getList(params, new AsyncCallback<JSONValue> () {
+		apiClient.notifications_getList(params, new AsyncCallback<List<Notification>> () {
 
 			public void onFailure(Throwable caught) {
 				handleFailure ( caught );
 			}
 
-			public void onSuccess(JSONValue result) {
+			public void onSuccess( List<Notification> result ) {
 				removeLoader ( outer );
 				
-				outer.add ( new HTML ( "<pre>" ) );
-				outer.add ( new HTML ( "" + result ) ) ;
-				outer.add ( new HTML ( "</pre>") ) ;
+				outer.add ( new HTML  ( "Result Size " + result.size() ) );
+				
+				for ( final  Notification no: result ) {
+					
+					VerticalPanel tmp = new VerticalPanel ();
+					tmp.addStyleName ( "notification" );
+					
+					
+					tmp.add ( new HTML ( "<h3>" + no.getTitleText() + "</h3>" ) );
+					tmp.add ( new HTML ( no.getBodyHtml() ) );
+					tmp.add ( new HTML ( no.getHref() ) );
+					outer.add ( tmp );
+				}
 			}
 			
 		});
