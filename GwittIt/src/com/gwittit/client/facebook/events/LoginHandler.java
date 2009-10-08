f636368548b357d283a92f47b9f1c0dd5bf1c9b7
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2009-10-08 22:51:23 +0000

    simplified events

--- GwittIt/src/com/gwittit/client/facebook/events/LoginHandler.java
+++ GwittIt/src/com/gwittit/client/facebook/events/LoginHandler.java
@@ -1,12 +1,7 @@
 package com.gwittit.client.facebook.events;
 
-
 import com.google.gwt.event.shared.EventHandler;
 
 public interface LoginHandler extends EventHandler {
-        /**
-         * The status of the Facebook connection has changed. For example the user
-         * has logged in or logged out.
-         */
-        public void loginStatusChanged(LoginEvent event);
+        public void onLogin ();
 }
