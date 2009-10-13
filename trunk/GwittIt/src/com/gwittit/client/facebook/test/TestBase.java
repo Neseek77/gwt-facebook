6610d5d699d6ebb2dfb7c7ec03ffa6726da69006
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2009-10-13 11:31:31 +0000

    not in use

--- trunk/GwittIt/src/com/gwittit/client/facebook/test/TestBase.java
+++
@@ -1,5 +0,0 @@
-package com.gwittit.client.facebook.test;
-
-public class TestBase {
-    public final String friends_areFriendsJson = "{\"0\":{\"uid1\":744450545, \"uid2\":709281400, \"are_friends\":true}, \"1\":{\"uid1\":751836969, \"uid2\":560635378, \"are_friends\":true}, \"2\":{\"uid1\":708775201, \"uid2\":709281400, \"are_friends\":false}}";
-}
