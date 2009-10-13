c11199c099028f7951ca68c3e32fc835735e01c2
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2009-10-13 14:46:42 +0000

    #gwt #facebook smartified code in FacebookApi.java

---
+++ GwittIt/src/com/gwittit/client/facebook/Callback.java
@@ -0,0 +1,37 @@
+package com.gwittit.client.facebook;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * Callback that by default logs the response.
+ */
+public class Callback<T> implements AsyncCallback<T> {
+
+    private AsyncCallback callback;
+    
+    
+    public Callback () {
+        
+    }
+    
+    public Callback ( AsyncCallback callback  ) {
+        this.callback = callback;
+    }
+    
+    public void onFailure(Throwable caught) {
+        
+        if ( callback != null ) {
+            callback.onFailure ( caught );
+        } else {
+            GWT.log ( "" + caught, null );
+        }
+    }
+
+    public void onSuccess(T result) {
+        GWT.log ( result + "" , null );
+    }
+    
+    
+
+}
