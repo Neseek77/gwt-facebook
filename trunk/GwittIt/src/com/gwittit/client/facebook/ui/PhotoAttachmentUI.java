58c2e9530372b4d944fb8d0dcf447560d047a0e3
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2009-10-16 08:14:19 +0000

    moved ui into facebook subpackage

---
+++ trunk/GwittIt/src/com/gwittit/client/facebook/ui/PhotoAttachmentUI.java
@@ -0,0 +1,12 @@
+package com.gwittit.client.facebook.ui;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.gwittit.client.facebook.entities.Attachment;
+
+public class PhotoAttachmentUI extends Composite {
+	
+	public PhotoAttachmentUI ( Attachment attachment ) {
+		
+	}
+
+}
