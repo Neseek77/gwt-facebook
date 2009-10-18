836b80092b3980416c8c235de03b13d13d5d29ee
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2009-10-18 22:12:20 +0000

    removed events handling, this is not the goal of this demo

--- trunk/GwittIt/src/com/gwittit/client/facebook/events/LoginHandler.java
+++
@@ -1,7 +0,0 @@
-package com.gwittit.client.facebook.events;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface LoginHandler extends EventHandler {
-        public void onLogin ();
-}
