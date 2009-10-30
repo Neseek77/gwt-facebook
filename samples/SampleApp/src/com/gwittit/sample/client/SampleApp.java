0172d755b27d41461d1535e1d1466115fe7ee518
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2009-10-30 07:50:09 +0000

    Improved connect status check

--- samples/SampleApp/src/com/gwittit/sample/client/SampleApp.java
+++ samples/SampleApp/src/com/gwittit/sample/client/SampleApp.java
@@ -12,9 +12,9 @@ import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.gwittit.client.facebook.ConnectState;
 import com.gwittit.client.facebook.FacebookApi;
 import com.gwittit.client.facebook.FacebookConnect;
-import com.gwittit.client.facebook.entities.SessionRecord;
 import com.gwittit.client.facebook.ui.ProfilePicsPanel;
 import com.gwittit.client.facebook.xfbml.Xfbml;
 
@@ -45,27 +45,49 @@ public class SampleApp implements EntryPoint {
     private final Anchor logoutLink = new Anchor ( "Logout") ;
     
     /*
+     * Outer
+     */
+    private final VerticalPanel outer = new VerticalPanel ();
+    /*
      * Add gui 
      */
-    private final VerticalPanel rootPanel = new VerticalPanel ();
+    private final VerticalPanel mainPanel = new VerticalPanel ();
     
+    private final HTML header = new HTML ( "<h1>gwt-facebook sample app</h1>" );
+    private final HTML waitingText = new HTML ( "Waiting for facebook connect state..." );
     /**
      *  Executed login when user clicks loginlink
      */
     private class LoginClick implements ClickHandler {
-
         public void onClick(ClickEvent event) {
             FacebookConnect.requireSession ( new AsyncCallback<Boolean> () {
                 public void onFailure(Throwable caught) {
                     Window.alert ( "Failed" );
                 }
                 public void onSuccess(Boolean result) {
-                    ensureInitAndRender();
+                    renderWhenConnected ();
                 }
             });
         }
     }
 
+    /**
+     * Fired when connect state is ready
+     */
+    private class WhenReady implements AsyncCallback<ConnectState> {
+        public void onFailure(Throwable caught) {
+            Window.alert ( "Failed to get facebook connect status, hit reload" );
+        }
+        public void onSuccess(ConnectState result) {
+            mainPanel.remove ( waitingText );
+            if ( result == ConnectState.connected ) {
+                renderWhenConnected ();
+            } else {
+                renderWhenNotConnected ();
+            }
+        }
+    };
+    
     /**
      * Executed when user logs out
      */
@@ -91,44 +113,37 @@ public class SampleApp implements EntryPoint {
      * Load app.
      */
     public void onModuleLoad() {
-        FacebookConnect.init ( API_KEY, "xd_receiver.htm", null );
-
         loginLink.addClickHandler ( new LoginClick ()  );
         logoutLink.addClickHandler ( new LogoutClick()  );
+
+        mainPanel.add ( waitingText );
         
-        ensureInitAndRender();
+        // Initialize Facebook Connect
+        FacebookConnect.init ( API_KEY );
+        // Wait until we can determine status, then render rest of the app.
+        FacebookConnect.waitUntilStatusReady ( new WhenReady() );
         
-        RootPanel.get ().add ( rootPanel );
+        outer.add ( header );
+        outer.add ( mainPanel );
+        RootPanel.get ().add ( outer );
     }
 
     /**
-     * Ensure connect lib is loaded before using it.
+     * UI rendered when connected
      */
-    public native void ensureInitAndRender () /*-{
-       var foo=this;
-       $wnd.FB.ensureInit(function(){
-            foo.@com.gwittit.sample.client.SampleApp::afterInit(Lcom/gwittit/client/facebook/entities/SessionRecord;)($wnd.FB.Facebook.apiClient.get_session());
-       });
-    
-    }-*/;
+    public void renderWhenConnected () {
+        mainPanel.clear ();
+        mainPanel.add ( logoutLink );
+        mainPanel.add ( new HTML ( " Your api key is " + apiClient.getApiKey ()  ) );
+        getFriends();
+    }
     
     /**
-     * Execute when connect lib has been initialized
+     * UI rendered when not connected
      */
-    public void afterInit ( SessionRecord sr ) {
-        
-        rootPanel.clear ();
-        
-        if ( !FacebookApi.sessionIsExpired ( sr ) ) {
-            // User has a valid session
-            rootPanel.add ( logoutLink );
-            rootPanel.add ( new HTML ( " Your api key is " + apiClient.getApiKey ()  ) );
-            getFriends();
-        } else {
-           
-            // No valid session
-            rootPanel.add ( loginLink );
-        }
+    public void renderWhenNotConnected () {
+        mainPanel.clear ();
+        mainPanel.add ( loginLink );
     }
         
     /**
@@ -144,8 +159,8 @@ public class SampleApp implements EntryPoint {
     public void renderFriends( List<Long> uids ) {
         ProfilePicsPanel ppp = new ProfilePicsPanel ( uids );
         ppp.getElement ().setId ( "FriendsGet" );
-        rootPanel.add ( new HTML ( "Your Friends: ") ) ;
-        rootPanel.add ( ppp );
+        mainPanel.add ( new HTML ( "Your Friends: ") ) ;
+        mainPanel.add ( ppp );
         Xfbml.parse ( ppp );
     }
 
