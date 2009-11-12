90fbfef53389c0d47dc60cc4a3a8d53d2d534b07
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2009-11-12 09:57:18 +0000

    doc

--- trunk/GwittIt/src/com/gwittit/client/facebook/entities/Media.java
+++ trunk/GwittIt/src/com/gwittit/client/facebook/entities/Media.java
@@ -3,78 +3,92 @@ package com.gwittit.client.facebook.entities;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.gwittit.client.facebook.Json;
 
-
 /**
  * Facebook Media
  * 
- * @see <a href="http://wiki.developers.facebook.com/index.php/Attachment_%28Streams%29">Attachments</a>
+ * @see <a
+ *      href="http://wiki.developers.facebook.com/index.php/Attachment_%28Streams%29">Attachments</a>
  * @author olamar72
  */
 public class Media extends JavaScriptObject {
-	
-    protected Media () {
-        
-    }
-    
-    public enum Type {image,hoto,link,video,flash,mp3};
-    
-    public final Type getTypeEnum () {
-        return Type.valueOf ( getType() );
+
+    protected Media() {
+
     }
 
-	/**
-	 * You can include rich media in the attachment for a post into a user's
-	 * stream. The media parameter contains a type, which can be one of
-	 * following: image, flash, mp3, or video; these media types render photos,
-	 * Flash objects, music, and video, respectively.
-	 */
-    public final native String getType() /*-{ return this.type; }-*/;
+    public enum Type {
+        image, hoto, link, video, flash, mp3
+    };
 
-	/**
-	 * The image media type is part an array which itself contains an array of
-	 * up to five JSON-encoded photo records. Each record must contain a src
-	 * key, which maps to the photo URL, and an href key, which maps to the URL
-	 * where a user should be taken if he or she clicks the photo.
-	 */
-    public final native String getSrc() /*-{ return this.src; }-*/;
+    public final Type getTypeEnum() {
+        return Type.valueOf ( getType () );
+    }
 
-	/**
-	 * Image alt/ preview link
-	 */
-    public final native String getAlt() /*-{ return this.alt; }-*/;
-	
-    public final native String getHref() /*-{ return this.href; }-*/;
+    /**
+     * You can include rich media in the attachment for a post into a user's
+     * stream. The media parameter contains a type, which can be one of
+     * following: image, flash, mp3, or video; these media types render photos,
+     * Flash objects, music, and video, respectively.
+     */
+    public final native String getType() /*-{
+        return this.type;
+    }-*/;
 
-	/**
-	 * Shich is the URL of the Flash object to be rendered.
-	 */
-    public final native String getSwfsrc() /*-{ return this.swfsrc; }-*/;
-   	
-    public final native String getImgsrc() /*-{ return this.imgsrc; }-*/;
-   
+    /**
+     * The image media type is part an array which itself contains an array of
+     * up to five JSON-encoded photo records. Each record must contain a src
+     * key, which maps to the photo URL, and an href key, which maps to the URL
+     * where a user should be taken if he or she clicks the photo.
+     */
+    public final native String getSrc() /*-{
+        return this.src;
+    }-*/;
+
+    /**
+     * Image alt/ preview link
+     */
+    public final native String getAlt() /*-{
+        return this.alt;
+    }-*/;
+
+    public final native String getHref() /*-{
+        return this.href;
+    }-*/;
+
+    /**
+     * Shich is the URL of the Flash object to be rendered.
+     */
+    public final native String getSwfsrc() /*-{
+        return this.swfsrc;
+    }-*/;
+
+    public final native String getImgsrc() /*-{
+        return this.imgsrc;
+    }-*/;
+
     /**
      * Video Object
      */
-    public final native Video getVideo() /*-{ return this.video; }-*/;
+    public final native Video getVideo() /*-{
+        return this.video;
+    }-*/;
 
-	
-    public final String stringify() {
-        return "Type:"  + getType();
-    }
-	
     /**
-     * Create new Media 
-     * @param src
-     * @param href
-     * @return
+     * Create new Media
+     * 
+     * @param src media
+     * @param href media
+     * @return new media object
      */
-    public static final Media newInstance ( Type type, String src, String href ) {
+    public static final Media newInstance(Type type, String src, String href) {
         Json j = new Json ();
         j.put ( "type", type.toString () );
         j.put ( "href", href ).put ( "src", src );
         return j.getJavaScriptObject ().cast ();
     }
 
-    public static native Media fromJson(String jsonString) /*-{ return eval('(' + jsonString + ')');}-*/;
+    public static native Media fromJson(String jsonString) /*-{
+        return eval('(' + jsonString + ')');
+    }-*/;
 
 }
