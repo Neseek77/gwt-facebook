7d982fdc5a9a056af4497ca0ea0382d092f05554
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2009-11-12 09:55:15 +0000

    added improved publish stream example

--- trunk/GwittIt/src/com/gwittit/client/facebook/entities/Attachment.java
+++ trunk/GwittIt/src/com/gwittit/client/facebook/entities/Attachment.java
@@ -2,22 +2,30 @@ package com.gwittit.client.facebook.entities;
 
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArray;
+import com.google.gwt.json.client.JSONObject;
+import com.gwittit.client.facebook.Json;
 
 
 /**
- * Wrapp attachment
- * 
+ * Wrapp attachment object. This object is either retreived from stream get calls,
+ * or you can publish it in our own stream
+ *  
  * @see http://wiki.developers.facebook.com/index.php/Attachment_%28Streams%29
- * @author ola
  * 
- * TODO: Let this class extend JavaScriptObject
+ * @author olamar72
+ * 
  */
 public class Attachment extends JavaScriptObject { 
+   
+    protected Attachment () { }
 
-    
-    protected Attachment () {
-    }
-
+    /**
+     * Set title of post.
+     * @param name
+     */
+    public final native void setName ( String name ) /*-{
+        this.name = name;
+    }-*/;
     
 	/**
 	 * The title of the post. The post should fit on one line in a user's
@@ -25,12 +33,24 @@ public class Attachment extends JavaScriptObject {
 	 */
     public final native String getName() /*-{ return this.name; }-*/;
 
+    /**
+     * Set attachment URL
+     * @param href URL
+     */
+    public final native void setHref ( String href ) /*-{ this.href=href; }-*/;
+    
 	/**
 	 * The URL to the source of the post referenced in the name. The URL should
 	 * not be longer than 1024 characters.
 	 */
     public final native String getHref() /*-{ return this.href; }-*/;
 
+    /**
+     * Set caption
+     * @param caption string
+     */
+    public final native void setCaption( String caption ) /*-{ this.caption=caption; }-*/;
+    
 	/**
 	 * A subtitle for the post that should describe why the user posted the item
 	 * or the action the user took. This field can contain plain text only, as
@@ -49,7 +69,18 @@ public class Attachment extends JavaScriptObject {
 	 * dictionary with 'text' and 'href' attributes.
 	 */
     public final native String getDescription() /*-{ return this.description; }-*/;
-
+    
+    /**
+     * Add media 
+     * @param m object
+     */
+    public final native void addMedia ( Media m ) /*-{ 
+        if ( typeof(this.media) == 'undefined' ) {
+            this.media = new Array();
+        }
+        this.media[this.media.length] = m;
+    }-*/;
+    
 	/**
 	 * Rich media that provides visual content for the post. media is an array that contains one of the following types: image, flash, mp3, or video, which are described below. Make sure you specify only one of these types in your post. 
 	 */
@@ -69,6 +100,8 @@ public class Attachment extends JavaScriptObject {
 	 */
     public final native String getIcon() /*-{ return this.icon; }-*/;
 	
+    public static native Attachment newInstance () /*-{ return eval ('({})') }-*/;
+    
 	public static native Attachment fromJson(String jsonString) /*-{ return eval('(' + jsonString + ')');}-*/;
 	
 }
