d123c5be22d4c7887300d41aeb965592daecf063
olamar72 <olamar72@ac6878b4-9caa-11de-b975-97273e741547>
2010-03-02 07:55:30 +0000

    Comments defaults to the xfbml version

--- trunk/GwittIt/src/com/gwittit/client/GwittIt.java
+++ trunk/GwittIt/src/com/gwittit/client/GwittIt.java
@@ -23,18 +23,18 @@ public class GwittIt implements EntryPoint {
     /**
      * Runs on every localhost port 8080
      */
-     //public static String API_KEY = "707cee0b003b01d52b2b6a707fa1202b";
+     // public static String API_KEY = "707cee0b003b01d52b2b6a707fa1202b";
 
     
      /**
       * Runs on every localhost port 8888
       */
-     public static String API_KEY = "1d81c942b38e2e6b3fc35a147d371ab3";
+     //public static String API_KEY = "1d81c942b38e2e6b3fc35a147d371ab3";
      
     /**
      * Change this if you setup your own app
      */
-     // public static String API_KEY = "aebf2e22b6bcb3bbd95c180bb68b6df4";
+     public static String API_KEY = "aebf2e22b6bcb3bbd95c180bb68b6df4";
 
     
     /*
